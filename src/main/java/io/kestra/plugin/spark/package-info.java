2657702083e14ec77d6bbc56c56ad241c96e4362
Loïc Mathieu <loikeseke@gmail.com>
2023-03-21 14:39:46 +0100

    feat: plugin categories and icons

---
+++ src/main/java/io/kestra/plugin/spark/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for submitting Apache Spark jobs.\n" +
+        "Apache Spark is a multi-language engine for executing data engineering, data science, and machine learning on single-node machines or clusters.",
+    categories = PluginSubGroup.PluginCategory.BATCH
+)
+package io.kestra.plugin.spark;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
