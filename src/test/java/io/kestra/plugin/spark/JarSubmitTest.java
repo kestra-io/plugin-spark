703284e40d690ee1b578fd36671d827833be34eb
Ludovic DEHON <tchiot.ludo@gmail.com>
2025-05-20 09:58:59 +0200

    chore(test): inject main tenants on storage and runOne

--- src/test/java/io/kestra/plugin/spark/JarSubmitTest.java
+++ src/test/java/io/kestra/plugin/spark/JarSubmitTest.java
@@ -8,6 +8,7 @@ import io.kestra.core.queues.QueueInterface;
 import io.kestra.core.runners.RunContext;
 import io.kestra.core.runners.RunContextFactory;
 import io.kestra.core.storages.StorageInterface;
+import io.kestra.core.tenant.TenantService;
 import io.kestra.core.utils.TestsUtils;
 import io.kestra.plugin.scripts.exec.scripts.models.DockerOptions;
 import io.kestra.plugin.scripts.exec.scripts.models.RunnerType;
@@ -46,7 +47,7 @@ class JarSubmitTest {
         URL resource = JarSubmitTest.class.getClassLoader().getResource("app.jar");
 
         URI put = storageInterface.put(
-            null,
+            TenantService.MAIN_TENANT,
             null,
             new URI("/file/storage/app.jar"),
             new FileInputStream(Objects.requireNonNull(resource).getFile())
