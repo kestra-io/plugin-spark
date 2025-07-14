728ea01c206c4eabc13f0479a297c06cdc8e4a79
Will Russell <wrussell@kestra.io>
2025-07-14 17:03:24 +0100

    fix: indentation and multiline method chaining

--- src/main/java/io/kestra/plugin/spark/SparkCLI.java
+++ src/main/java/io/kestra/plugin/spark/SparkCLI.java
@@ -38,38 +38,35 @@ import jakarta.validation.constraints.NotNull;
             namespace: company.team
 
             tasks:
-                - id: hello
-                  type: io.kestra.plugin.spark.SparkCLI
-                  inputFiles:
-                    pi.py: |
-                      import sys
-                      from random import random
-                      from operator import add
-                      from pyspark.sql import SparkSession
+              - id: hello
+                type: io.kestra.plugin.spark.SparkCLI
+                inputFiles:
+                  pi.py: |
+                    import sys
+                    from random import random
+                    from operator import add
+                    from pyspark.sql import SparkSession
 
-                      if __name__ == "__main__":
-                          spark = SparkSession \
-                              .builder \
-                              .appName("PythonPi") \
-                              .getOrCreate()
+                    if __name__ == "__main__":
+                        spark = SparkSession.builder.appName("PythonPi").getOrCreate()
 
-                          partitions = int(sys.argv[1]) if len(sys.argv) > 1 else 2
-                          n = 100000 * partitions
+                        partitions = int(sys.argv[1]) if len(sys.argv) > 1 else 2
+                        n = 100000 * partitions
 
-                          def f(_: int) -> float:
-                              x = random() * 2 - 1
-                              y = random() * 2 - 1
-                              return 1 if x ** 2 + y ** 2 <= 1 else 0
+                        def f(_: int) -> float:
+                            x = random() * 2 - 1
+                            y = random() * 2 - 1
+                            return 1 if x ** 2 + y ** 2 <= 1 else 0
 
-                          count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
-                          print("Pi is roughly %f" % (4.0 * count / n))
+                        count = spark.sparkContext.parallelize(range(1, n + 1), partitions).map(f).reduce(add)
+                        print("Pi is roughly %f" % (4.0 * count / n))
 
-                          spark.stop()
-                  containerImage: bitnami/spark
-                  taskRunner:
-                    networkMode: host
-                  commands:
-                    - spark-submit --name Pi --master spark://localhost:7077 pi.py"""
+                        spark.stop()
+                containerImage: bitnami/spark
+                taskRunner:
+                  networkMode: host
+                commands:
+                  - spark-submit --name Pi --master spark://localhost:7077 pi.py"""
         )
     }
 )
